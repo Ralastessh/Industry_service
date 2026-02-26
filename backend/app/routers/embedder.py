212c277135f73ed3dadee6ed701a315c609cf354
Ralastessh <nagabuti13@hanyang.ac.kr>
2026-02-26 15:07:20 +0900

    fix: update embedder.py

--- backend/app/routers/embedder.py
+++ backend/app/routers/embedder.py
@@ -6,5 +6,5 @@ def get_embedder():
     load_dotenv()
     model = os.getenv("OPENAI_EMBEDDING", "text-embedding-3-small")
     api_key = os.getenv("OPENAI_API_KEY")
-    # client 생성 후 embedding 가능하게 만들어주는 wrapper 클래 -> Langchain vectorstore 형식에 맞는 list 반환
+    # client 생성 후 embedding 가능하게 만들어주는 wrapper 클래스 -> Langchain vectorstore 형식에 맞는 list 반환
     return OpenAIEmbeddings(model=model, api_key=api_key)
